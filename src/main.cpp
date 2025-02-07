eeb88a5a24c912a3d72ed2fecf2d6b579c6a58bd
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-02-06 20:27:30 -0500

    Add SDL using vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include "SDL3/SDL.h"
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Test", 640, 480, NULL);
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
