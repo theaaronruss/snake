6f9b27f97676c4a54e7e84a99c168e5c8f617e09
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-10-21 12:43:47 -0400

    draw snake to screen with movement controls

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/theaaronruss/snake
 
 go 1.25.2
+
+require golang.org/x/term v0.36.0
+
+require golang.org/x/sys v0.37.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
+
+	"golang.org/x/term"
 )
+
+type point struct {
+	x int
+	y int
+}
+
+type snake struct {
+	body []point
+	dirX int
+	dirY int
+}
 
 const (
 	fps         int = 15
@@ -16,56 +27,102 @@ const (
 	pixelHeight int = height * 4
 )
 
-type point struct {
-	x int
-	y int
+var (
+	screen = make([]byte, width*height)
+	player = newSnake()
+)
+
+func newSnake() *snake {
+	s := snake{
+		body: make([]point, 5),
+		dirX: 1,
+	}
+	s.body[0] = point{x: 15, y: 15}
+	s.body[1] = point{x: 14, y: 15}
+	s.body[2] = point{x: 13, y: 15}
+	s.body[3] = point{x: 12, y: 15}
+	s.body[4] = point{x: 11, y: 15}
+	return &s
 }
 
-var screen = make([]byte, width*height)
-var snake []point
+func (s *snake) move() {
+	length := len(s.body)
+	s.body = s.body[1:]
+	head := s.body[length-2]
+	newPoint := point{x: head.x + s.dirX, y: head.y + s.dirY}
+	s.body = append(s.body, newPoint)
+}
 
 func main() {
-	snake = append(snake, point{x: 11, y: 15})
-	snake = append(snake, point{x: 12, y: 15})
-	snake = append(snake, point{x: 13, y: 15})
-	snake = append(snake, point{x: 14, y: 15})
-	snake = append(snake, point{x: 15, y: 15})
-	for _, p := range snake {
-		setPixel(p.x, p.y)
-	}
+	fd := int(os.Stdin.Fd())
 
-	// hide cursor
-	fmt.Print("\033[?25l")
-
-	// show cursor on exit
-	defer fmt.Print("\033[?25h")
-
-	// make sure cursor is shown again even when not exiting normally
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		fmt.Print("\033[?25h")
+	if !term.IsTerminal(fd) {
+		fmt.Fprintln(os.Stderr, "Please run from a TTY")
 		os.Exit(1)
-	}()
+	}
 
-	for {
-		startTime := time.Now()
-		drawScreen()
-		clearSnake()
-		head := snake[len(snake)-1]
-		// fmt.Printf("X: %d Y: %d\n", head.x, head.y)
-		snake = snake[1:]
-		// fmt.Printf("len: %d\n", len(snake))
-		snake = append(snake, point{x: head.x + 1, y: head.y})
-		drawSnake()
-		elapsedTime := time.Since(startTime)
-		sleepTime := 1000.0/fps - int(elapsedTime.Milliseconds())
-		time.Sleep(time.Millisecond * time.Duration(sleepTime))
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to change TTY to raw mode")
+		os.Exit(1)
+	}
+	defer term.Restore(fd, oldState)
+
+	drawBorder()
+
+	c := make(chan byte)
+	go handleInput(c)
+	done := false
+	fmt.Print("\033[?25l")     // hide cursor
+	fmt.Print("\033[H\033[2J") // clear screen
+	for !done {
+		select {
+		case input, ok := <-c:
+			if !ok {
+				done = true
+			}
+			if input == 'j' {
+				player.dirX = 0
+				player.dirY = 1
+			}
+			if input == 'k' {
+				player.dirX = 0
+				player.dirY = -1
+			}
+			if input == 'h' {
+				player.dirX = -1
+				player.dirY = 0
+			}
+			if input == 'l' {
+				player.dirX = 1
+				player.dirY = 0
+			}
+		default:
+			startTime := time.Now()
+			eraseSnake()
+			player.move()
+			drawSnake()
+			renderScreen()
+			elapsedTime := time.Since(startTime)
+			frameTime := time.Duration(1000.0/fps) * time.Millisecond
+			time.Sleep(frameTime - elapsedTime)
+		}
+	}
+	fmt.Print("\033[?25h") // show cursor
+}
+
+func drawBorder() {
+	for i := range pixelWidth {
+		setPixel(i, 0, true)
+		setPixel(i, pixelHeight-1, true)
+	}
+	for i := 1; i < pixelHeight-1; i++ {
+		setPixel(0, i, true)
+		setPixel(pixelWidth-1, i, true)
 	}
 }
 
-func setPixel(x int, y int) {
+func setPixel(x int, y int, on bool) {
 	if x < 0 || x >= pixelWidth || y < 0 || y >= pixelHeight {
 		return
 	}
@@ -74,58 +131,53 @@ func setPixel(x int, y int) {
 	groupY := y / 4
 	dotX := x % 2
 	dotY := y % 4
-	dot := screen[groupY*width+groupX]
-	dot |= 0x80 >> (dotY*2 + dotX)
-	screen[groupY*width+groupX] = dot
-}
 
-func clearPixel(x int, y int) {
-	if x < 0 || x >= pixelWidth || y < 0 || y >= pixelHeight {
-		return
+	pixelGroup := screen[groupY*width+groupX]
+	var mask byte = 0x80 >> (dotY*2 + dotX)
+	if on {
+		pixelGroup |= mask
+	} else {
+		pixelGroup &= ^mask
 	}
-
-	groupX := x / 2
-	groupY := y / 4
-	dotX := x % 2
-	dotY := y % 4
-	dot := screen[groupY*width+groupX]
-	var change byte = 0x80 >> (dotY*2 + dotX)
-	dot &= ^change
-	screen[groupY*width+groupX] = dot
+	screen[groupY*width+groupX] = pixelGroup
 }
 
-func clearSnake() {
-	for _, p := range snake {
-		clearPixel(p.x, p.y)
+func renderScreen() {
+	fmt.Printf("\033[%dA", height+1)
+	for y := range height {
+		for x := range width {
+			char := screen[y*width+x]
+			fmt.Printf("%c", pixelGroupRune(char))
+		}
+		fmt.Print("\r\n")
+	}
+}
+
+func eraseSnake() {
+	for _, p := range player.body {
+		setPixel(p.x, p.y, false)
 	}
 }
 
 func drawSnake() {
-	for _, p := range snake {
-		setPixel(p.x, p.y)
+	for _, p := range player.body {
+		setPixel(p.x, p.y, true)
 	}
 }
 
-func drawScreen() {
-	for i := range pixelWidth {
-		setPixel(i, 0)
-		setPixel(i, pixelHeight-3)
-	}
-	for i := 1; i < pixelHeight-2; i++ {
-		setPixel(0, i)
-		setPixel(pixelWidth-1, i)
-	}
-
-	// move cursor up to redraw screen
-	fmt.Printf("\033[%dA", height+1)
-
-	for y := range height {
-		for x := range width {
-			pixelGroup := screen[y*width+x]
-			fmt.Printf("%c", pixelGroupRune(pixelGroup))
+func handleInput(c chan<- byte) {
+	done := false
+	buf := make([]byte, 1)
+	for !done {
+		os.Stdin.Read(buf)
+		input := buf[0]
+		if input == 'q' {
+			done = true
+		} else {
+			c <- buf[0]
 		}
-		fmt.Println()
 	}
+	close(c)
 }
 
 func pixelGroupRune(pixelGroup byte) rune {
